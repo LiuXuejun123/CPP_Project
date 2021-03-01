8e573a4927b2dc25ec5a786fb09b06350341cdfb
LiuXuejun123 <liuxuejun0855@163.com>
2021-03-01 18:47:28 +0800

    carrentalsystem

---
+++ Car Rental System/cmake-build-debug/CMakeFiles/Car_Rental_System.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Car_Rental_System.dir/main.cpp.obj"
+  "Car_Rental_System.exe"
+  "Car_Rental_System.exe.manifest"
+  "Car_Rental_System.pdb"
+  "libCar_Rental_System.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Car_Rental_System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
